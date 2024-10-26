bf9843830328ea7f50c7bf616cca76143b0f2743
mahi <mahi@gmail.com>
2024-10-26 22:53:24 +0530

    Reapply "bike update updated"
    This reverts commit 514004267f54636b9cab46f65bca7d1834bbe3a5.

--- javacode.java
+++ javacode.java
@@ -9,7 +9,7 @@ public class SimplePyramid {
             }
             // Print asterisks
             for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("*");
+                System.out.print("Mani");
             }
             // Move to the next line
             System.out.println();
